8c5747ea5919c451824d1b6396f9df741f0bd1ce
Your Name <you@example.com>
2025-12-01 21:51:15 +0000

    Added Shiny App Folder - 12/1/2025, 4:51 PM - Stephen

---
+++ Prediction Model Shiny App/app.R
@@ -0,0 +1,16 @@
+#THIS IS THE EXAMPLE COPIED FROM THE LECTURE
+
+library(shiny)
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+
+server <- function(input, output, session) {
+  output$greeting <- renderText({
+    paste0("Hello ", input$name, "!")
+  })
+}
+
+shinyApp(ui = ui, server = server)
