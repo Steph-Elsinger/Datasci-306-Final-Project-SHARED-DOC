646a0ec43d1dd5d61488af298b7a918452560d90
Stephen Elsinger <stephels@umich.edu>
2025-12-04 22:14:53 +0000

    Shiny App

--- Prediction Model Shiny App/app.R
+++ Prediction Model Shiny App/app.R
@@ -1,16 +1,38 @@
-#THIS IS THE EXAMPLE COPIED FROM THE LECTURE
-
 library(shiny)
 
 ui <- fluidPage(
-  textInput("name", "What's your name?"),
-  textOutput("greeting")
+  titlePanel("What Are Your Odds of Being a Smoker in the US in 2025?")
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        "race",
+        "What is your race?"
+        choices = unique(tobacco_region$Demographic == `Race & Ethnicity`)),
+      selectInput(
+        "region",
+        "Where in the US do you live"
+        choices = unique(tobacco_region$region)),
+      selectInput(
+        "income",
+        "How much do you make anually?"
+        choices = unique(tobacco_region$Demographic == `Income`)),
+      selectInput(
+        "employment",
+        "How would you describe your employment status?"
+        choices = unique(tobacco_region$Demographic == `Employment`)),
+      selectInput(
+        "age",
+        "How old are you?"
+        choices = unique(tobacco_region$Demographic == `Age`)),
+      selectInput(
+        "mentalhealth",
+        "How would you describe the state of your mental health?"
+        choices = unique(tobacco_region$Demographic == `Mental Health`)),
+      actionButton("go", "Calculate My Odds")),
+    
+    mainPanel(textOutput("predictionText"))
+  )
 )
 
 server <- function(input, output, session) {
-  output$greeting <- renderText({
-    paste0("Hello ", input$name, "!")
-  })
-}
-
-shinyApp(ui = ui, server = server)
+  
